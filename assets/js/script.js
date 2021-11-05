30ecb1914c01ae8f6719a19adfadaacd2eea4e6b
isidora <delrio.isi@gmail.com>
2021-11-05 01:13:52 -0300

    funciones javascrip y botones seccion destacados

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// notas js idrl
+
+$(function () {
+   $("[data-toggle='tooltip']").tooltip(); 
+
+   $("#graciasporconocernos, .btn btn-outline-warning").click(function (){
+    alert("gracias por conocernos");
+  });
+
+
+});
+
